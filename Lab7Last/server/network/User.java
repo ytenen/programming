d059e155bff18a3d7f6cd7c8e5d969e88990b1ee
ytenen <ytenene@gmail.com>
2024-05-18 22:20:33 +0300

    user fix

--- Lab7Last/server/network/User.java
+++ Lab7Last/server/network/User.java
@@ -11,12 +11,6 @@ public class User implements Serializable {
 
     private String password;
 
-
-
-
-
-
-
     public User(String login, String password) {
         this.login = login;
         this.password = password;
@@ -42,16 +36,4 @@ public class User implements Serializable {
         this.password = password;
     }
 
-    @Override
-    public boolean equals(Object o) {
-        if (this == o) return true;
-        if (o == null || getClass() != o.getClass()) return false;
-        User user = (User) o;
-        return Objects.equals(login, user.login) && Objects.equals(password, user.password);
-    }
-
-    @Override
-    public int hashCode() {
-        return Objects.hash(login, password);
-    }
 }
